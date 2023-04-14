1dc1b859d830a9eece9b740903b1b35220d842e6
Abraham Adeboye <abrahamadeboye00@gmail.com>
2023-04-14 23:03:00 +0100

    new file:   algebra.py 	new file:   graph.py 	modified:   index.py

---
+++ algebra.py
@@ -0,0 +1,10 @@
+from math import *
+
+# Coded by Abraham Adeboye
+# In this program, we will do some basic and advance algebra with Python
+# so let's get started
+
+# Variables
+a = 3
+b = 1
+c = 2
--- index.py
+++ index.py
@@ -1,6 +1,17 @@
+import random
+import string
+from math import *
+
 # This is a python file that explains most basic functions, tags, snippet and syntax. Be sure you understand this
 # before moving on to the advanced "coded" file. 
 # You can visit this link to learn more >> "https://www.python.org/about/gettingstarted/"
 
 
 print("Hello baby, Let's get started in Programming with Pyhton3")
+
+
+def generate_transaction_code():
+    letters = string.ascii_uppercase
+    digits = string.digits
+    code = ''.join(random.choice(letters + digits) for i in range(12))
+    return code
