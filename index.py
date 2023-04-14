f78f0a7d9cbb71fd56fec7250d24812999c845f0
Abraham Adeboye <abrahamadeboye00@gmail.com>
2023-04-14 22:32:09 +0100

    modified:   sums.py

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
 # This is a python file that explains most basic functions, tags, snippet and syntax. Be sure you understand this
-# before moving on to the advanced "coded" file
+# before moving on to the advanced "coded" file. 
+# You can visit this link to learn more >> "https://www.python.org/about/gettingstarted/"
 
+
+print("Hello baby, Let's get started in Programming with Pyhton3")
--- sums.py
+++ sums.py
@@ -1,11 +1,49 @@
 #Coded by AbrahamAdeboye (Elitepreneur)
-
+#Modules
 from math import *  # This is simply to import all functions from a python Mathematics module.
 
-print("The addition of 3 and 7 is:") # Print is just a basic python commmand telling it to print a certain 
-print(3 + 7) # This is simply telling the python interpreter to add 3 with 7
 
 
-#Simple Subtraction
-print("The subtraction of 2 from 4 is:")
-print(4 - 2) 
+
+# variables
+x = 4
+y = 8
+z = 3
+a = 10
+
+print("In this program file, we have four variables and we are gonna perform different sums operation on them.")
+print("The variables are as follows: x = 4, y = 8, z = 3 and a = 10")
+
+# Additions
+
+print("Starting with additions, lets add 'a' with 'y'.")
+additions = (y + a)
+additions = additions.__str__()
+print("Which is " + additions)
+
+# Subtraction
+print("We will subtract 'z' from 'x'.")
+subtraction = x - z
+subtraction = subtraction.__str__()
+print("Answer is " + subtraction)
+
+# Multiplication
+print("Moving on with multiplication.")
+multiplication = x * y
+multiplication = multiplication.__str__()
+print("Multiplying 'x' and 'y' will give us " + multiplication)
+
+# Division
+print("and the division goes with 'y' divided by 'x'.")
+division = y / x
+division = division.__str__()
+print("And that will be 8 divided by 4 which is " + division)
+
+
+# Modulus
+print("And lastly, we will be treating Modulus commonly know as Mod.")
+print("Let's just use 'a' and 'z' in this case.")
+mod = a % z
+mod = mod.__str__()
+print("Which will be 10 mod 3.")
+print("Answer is " + mod + ".")
