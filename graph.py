de13bd0f02d10a972bfd39ce45bd5d8c39b85504
Abraham Adeboye <abrahamadeboye00@gmail.com>
2023-04-14 23:07:09 +0100

    modified:   graph.py

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+# Coded by Abraham Adeboye
 
 x = [1, 2, 3, 4, 5]
 y = [10, 5, 20, 15, 25]
@@ -9,4 +10,4 @@ plt.plot(x, y)
 plt.title("Line Graph Example")
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
- plt.show()
+plt.show()
